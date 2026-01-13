9b4b6a6df54f14b4e2172656be63e5545e2cafab
Eunock <izumishinishi7@gmail.com>
2026-01-13 01:07:19 +0100

    Ajout des dependances et restructuration de l'achitecture du projet

--- src/controllers/auth/user.controller.js
+++ src/controllers/auth/user.controller.js
@@ -1,4 +1,4 @@
-import { UserService } from "#services/user.service";
+import { UserService } from "#services/auth/user.service";
 import { UserDto } from "#dto/user.dto";
 import { signToken } from "#lib/jwt";
 import { validateData } from "#lib/validate";

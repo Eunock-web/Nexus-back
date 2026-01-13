40ee2e1180dd8f873d25932d47febc545db241e7
Eunock <izumishinishi7@gmail.com>
2026-01-13 08:21:39 +0100

    feat(Schema) : Mis en place du schema de validation du register

---
+++ src/schemas/auth/register.schema.js
@@ -0,0 +1,28 @@
+import { email, optional, z } from 'zod';
+
+
+// Taille max : 5Mo (exemple)
+const MAX_FILE_SIZE = 5 * 1024 * 1024;
+const ACCEPTED_IMAGE_TYPES = ["image/jpeg", "image/jpg", "image/png", "image/webp"];
+
+
+const registerSchema = z.object({
+
+  email : z.string().email('Email invalide'),
+  password : z.string()
+              .min(8, "Il faut au moins 8 caractères")       
+              .regex(/[A-Z]/, "Il faut au moins un majuscule")       
+              .regex(/[a-z]/, "Il faut au moins un minuscule")       
+              .regex(/[0-9]/, "Il faut au moins un nombre"),
+              
+  firstname : z.string().min(3, "Minimum 3 caractères").optional(),
+  lastname : z.string().min(3, "Minimum 3 caractères").optional(),
+  avatarUrl : z.any()
+               .refine((file) => file?.size <= MAX_FILE_SIZE, `Taille max : 5Mo.`)
+               .refine(
+                  (file) => ACCEPTED_IMAGE_TYPES.includes(file?.type),
+                  "Seuls les formats .jpg, .jpeg, .png et .webp sont acceptés."
+                )
+              .optional(),
+               
+});

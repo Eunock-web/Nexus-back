faf31a3c8e83671c2b8e328ea160dcd3f8d6afbd
Eunock <izumishinishi7@gmail.com>
2026-01-16 18:29:22 +0100

    feat: Mis en place du schema de validation zod du loginwxc*ty

--- src/schemas/auth/login.schema.js
+++ src/schemas/auth/login.schema.js
@@ -2,5 +2,9 @@ import { z } from 'zod'
 
 export const loginSchema = z.object({
   email: z.string().email("Email invalide"),
-  password: z.string().min(1, "Mot de passe requis"),
+  password : z.string()
+              .min(8, "Il faut au moins 8 caractères")       
+              .regex(/[A-Z]/, "Il faut au moins un majuscule")       
+              .regex(/[a-z]/, "Il faut au moins un minuscule")       
+              .regex(/[0-9]/, "Il faut au moins un nombre"),
 });

77bb6d1042cdc0260ee62504fa66559da49f9ee9
Eunock <izumishinishi7@gmail.com>
2026-01-26 17:15:12 +0100

    fix: Corection de l'erreur a propos de la favicon

--- src/index.js
+++ src/index.js
@@ -24,6 +24,7 @@ app.use(helmet());
 app.use(cors());
 app.use(httpLogger);
 app.use(express.json());
+app.get('/favicon.ico', (req, res) => res.status(204).end());
 
 // Documentation Swagger
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(specs, {

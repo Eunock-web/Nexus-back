d7a611fd127e6ed5bad28a19c8ffa5e7e1ecebcf
Eunock <izumishinishi7@gmail.com>
2026-02-13 08:58:29 +0100

    fix: Modification du chemain de la OAuth route

--- src/index.js
+++ src/index.js
@@ -55,7 +55,7 @@ app.get("/", (req, res) => {
 
 // Utilisation des routes
 app.use("/api", userRouter); // Pour garder /register et /login à la racine
-app.use("/Oauth", oauthRouter); // Routes OAuth
+app.use("api/Oauth", oauthRouter); // Routes OAuth
 app.use("/api/workspace", workspaceRouter); // Routes Workspace
 app.use("/api/project", workspaceRouter); // Routes project
 
